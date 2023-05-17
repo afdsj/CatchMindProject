2d1da276165d45ffd5ba27d018c34da6a7a5133b
asdfj <thdla3441@gmail.com>
2023-05-17 22:06:50 +0900

    project

---
+++ CatchMind/src/gui/listenAdapter.java
@@ -0,0 +1,41 @@
+package gui;
+
+//main에 리스너를 넣기위한 어댑터
+public abstract class listenAdapter implements KeyListener, MouseListener, ActionListener{
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
+}
